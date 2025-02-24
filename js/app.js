c9d550ae84abec095d3e01b09aeb1f4396a3ce04
Val <60014136+voffee@users.noreply.github.com>
2025-02-24 17:39:02 +0800

    JS spinner completed, ffw functionality in progress.

--- js/app.js
+++ js/app.js
@@ -6,20 +6,31 @@ const ffw = document.querySelector('.ffw');
 
 const spinnerArc = document.querySelector('.spinner-segment');
 
+let Speed = getComputedStyle(document.documentElement).getPropertyValue('--spinner-duration');
+
 const stop2 = 5;
 // Add Event Listeners
 stop.addEventListener('click', stopAnimation);
 play.addEventListener('click', playAnimation)
-// ffw.addEventListener('click', ffwAnimation);
+ffw.addEventListener('click', ffwAnimation);
 
 // Create Functions
 function stopAnimation() {
     spinnerArc.classList.remove('rotate-switch');
+
 }
 
 function playAnimation() {
     spinnerArc.classList.add('rotate-switch');
 }
 
+function ffwAnimation() {
+    setSpeed();
+}
+
+function setSpeed() {
+    document.documentElement.style.setProperty('--spinner-duration', '0.1s');
+}
+
 // TEST SPACE
-console.log(stop2);
+// console.log(stop2);
