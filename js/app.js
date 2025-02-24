f41dbf2b36dbb7683bea0e50f187381a269543d6
Val <60014136+voffee@users.noreply.github.com>
2025-02-24 15:55:41 +0800

    CSS Completed & Basic JS partial completion.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 
 // Define UI Variables
+const stop = document.querySelector('.stop');
+const play = document.querySelector('.play');
+const ffw = document.querySelector('.ffw');
 
+const spinnerArc = document.querySelector('.spinner-segment');
+
+const stop2 = 5;
 // Add Event Listeners
+stop.addEventListener('click', stopAnimation);
+play.addEventListener('click', playAnimation)
+// ffw.addEventListener('click', ffwAnimation);
 
 // Create Functions
+function stopAnimation() {
+    spinnerArc.classList.remove('rotate-switch');
+}
+
+function playAnimation() {
+    spinnerArc.classList.add('rotate-switch');
+}
+
+// TEST SPACE
+console.log(stop2);
